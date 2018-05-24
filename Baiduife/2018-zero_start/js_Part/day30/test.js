76423e59546c2efaba0737aa8e143c7d8d1a755c
LeoMalik <LeoMalik@users.noreply.github.com>
2018-05-25 00:28:59 +0800

    null

--- Baiduife/2018-zero_start/js_Part/day30/test.js
+++ Baiduife/2018-zero_start/js_Part/day30/test.js
@@ -7,26 +7,22 @@ $(document).ready(function(){
     var checkAll_product=$('#product .checkAll');
     var inputList_region=$("#region input:not('.checkAll')");
     var inputList_product=$("#product input:not('.checkAll')");
-    // true代表全选
-    var flag_region=true;
-
+    
     checkAll_region.click(function(){
-        // 获取全选以外的列表
-        if(flag_region==true){
-            inputList_region.each(function(){
-                $(this).attr('checked','true');              
+        if(this.checked==true){
+             inputList_region.each(function(){
+                $(this).prop('checked',true);              
             });
         }
         else{
             inputList_region.each(function(){
-                $(this).removeAttr('checked');  
+                $(this).prop('checked',false);
             });
         }
-        flag_region=!flag_region;
-    });
+    })
 
     region.delegate("input:not('.checkAll')",'click',function(){
-        // 统计选中数目
+        // 统计选中数目s
         var checkCount=0;
         // 未选中的数目
         var notCheckCount=0;
@@ -41,12 +37,11 @@ $(document).ready(function(){
                 notCheckCount++;
             }
             // 全选框的操作
-            (checkCount==inputList_region.length) ? checkAll_region.attr('checked','true') : checkAll_region.removeAttr('checked');
+            (checkCount==inputList_region.length) ? checkAll_region.prop('checked',true) : checkAll_region.prop('checked',false);
             // 只要一个选中
             if(notCheckCount==inputList_region.length){
                 currentItem.checked = true;
             }
         })
-        
     })
 });

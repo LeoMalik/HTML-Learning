6919509f217f2bcc95f023e74111d67671d4d306
LeoMalik <LeoMalik@users.noreply.github.com>
2018-05-27 18:22:20 +0800

    null

--- Baiduife/2018-zero_start/js_Part/day30/test.js
+++ Baiduife/2018-zero_start/js_Part/day30/test.js
@@ -1,43 +1,3 @@
-// 测试数据
-let productData = [{
-    product: "手机",
-    region: "华东",
-    sale: [120, 100, 140, 160, 180, 185, 190, 210, 230, 245, 255, 270]
-}, {
-    product: "手机",
-    region: "华北",
-    sale: [80, 70, 90, 110, 130, 145, 150, 160, 170, 185, 190, 200]
-}, {
-    product: "手机",
-    region: "华南",
-    sale: [220, 200, 240, 250, 260, 270, 280, 295, 310, 335, 355, 380]
-}, {
-    product: "笔记本",
-    region: "华东",
-    sale: [50, 60, 80, 110, 30, 20, 70, 30, 420, 30, 20, 20]
-}, {
-    product: "笔记本",
-    region: "华北",
-    sale: [30, 35, 50, 70, 20, 15, 30, 50, 710, 130, 20, 20]
-}, {
-    product: "笔记本",
-    region: "华南",
-    sale: [80, 120, 130, 140, 70, 75, 120, 90, 550, 120, 110, 100]
-}, {
-    product: "智能音箱",
-    region: "华东",
-    sale: [10, 30, 4, 5, 6, 5, 4, 5, 6, 5, 5, 25]
-}, {
-    product: "智能音箱",
-    region: "华北",
-    sale: [15, 50, 15, 15, 12, 11, 11, 12, 12, 14, 12, 40]
-}, {
-    product: "智能音箱",
-    region: "华南",
-    sale: [10, 40, 10, 6, 5, 6, 8, 6, 6, 6, 7, 26]
-}]
-
-
 
 // 主体程序
 $(document).ready(function () {
@@ -47,8 +7,6 @@ $(document).ready(function () {
     // 全选标签
     var checkAll_region = $('#region .checkAll');
     var checkAll_product = $('#product .checkAll');
-    var inputList_region = $("#region input:not('.checkAll')");
-    var inputList_product = $("#product input:not('.checkAll')");
     // 获取用户提交的查询
     getSearchInput();
 
@@ -186,6 +144,46 @@ function getSearchInput() {
         order: flag,
         rowspan: Rowspan
     }
+
+    // // 测试数据
+    // let productData = [{
+    //     product: "手机",
+    //     region: "华东",
+    //     sale: [120, 100, 140, 160, 180, 185, 190, 210, 230, 245, 255, 270]
+    // }, {
+    //     product: "手机",
+    //     region: "华北",
+    //     sale: [80, 70, 90, 110, 130, 145, 150, 160, 170, 185, 190, 200]
+    // }, {
+    //     product: "手机",
+    //     region: "华南",
+    //     sale: [220, 200, 240, 250, 260, 270, 280, 295, 310, 335, 355, 380]
+    // }, {
+    //     product: "笔记本",
+    //     region: "华东",
+    //     sale: [50, 60, 80, 110, 30, 20, 70, 30, 420, 30, 20, 20]
+    // }, {
+    //     product: "笔记本",
+    //     region: "华北",
+    //     sale: [30, 35, 50, 70, 20, 15, 30, 50, 710, 130, 20, 20]
+    // }, {
+    //     product: "笔记本",
+    //     region: "华南",
+    //     sale: [80, 120, 130, 140, 70, 75, 120, 90, 550, 120, 110, 100]
+    // }, {
+    //     product: "智能音箱",
+    //     region: "华东",
+    //     sale: [10, 30, 4, 5, 6, 5, 4, 5, 6, 5, 5, 25]
+    // }, {
+    //     product: "智能音箱",
+    //     region: "华北",
+    //     sale: [15, 50, 15, 15, 12, 11, 11, 12, 12, 14, 12, 40]
+    // }, {
+    //     product: "智能音箱",
+    //     region: "华南",
+    //     sale: [10, 40, 10, 6, 5, 6, 8, 6, 6, 6, 7, 26]
+    // }]
+
     // // 利用datas去重,判断勾选地区和产品的数量,同样可以达到判断排序方式和rowspan的问题,这样就可以传入一组数据直接开搞(懒了,省略)
     // var regionArray=[];
     // var productArray=[];
@@ -220,7 +218,7 @@ function showTable(datas) {
     var rowspan = datas.rowspan;
     var innerHtml1 = "";
     var rowspanArray = [];
-    var rowspanIndex = 1;
+    var tableTitle="";
     if (order == true) {
         tableTitle = "<thead class='bg-info'><th>地区</th><th>商品</th><th>1月</th><th>2月</th><th>3月</th><th>4月</th><th>5月</th><th>6月</th><th>7月</th><th>8月</th><th>9月</th><th>10月</th><th>11月</th><th>12月</th></thead>";
         // 按照产品对应三个地区来排序
